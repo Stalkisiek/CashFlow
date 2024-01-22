2426c264225a5dfac5e9504cbabe27ce13a59051
Stalkisiek <matmigdal@gmail.com>
2024-01-22 02:18:32 +0100

    Added is_admin cookie

--- Frontend/cashflow/src/features/login/api.ts
+++ Frontend/cashflow/src/features/login/api.ts
@@ -2,11 +2,21 @@ import { useCookies } from "react-cookie";
 import { API_URL } from "../../config";
 import {ServiceResponse} from "../../types/ServiceResponse";
 import Swal from "sweetalert2";
+import {User} from "../../types/User";
+
+const headersCreator = (jwt:string) => {
+    return {
+        'Authorization' : `Bearer ${jwt}`,
+        'Content-Type': 'application/json',
+    };
+}
 
 export const useLoginApi = () => {
     const authCookieName = 'auth_token';
-    const [, setCookie] = useCookies([authCookieName]);
-
+    const isAdminCookieName = 'is_admin';
+    const [, setCookie] = useCookies([authCookieName, isAdminCookieName]);
+    const tokenName = 'auth_token';
+    const[cookies] = useCookies([tokenName]);
     return async (email: string, password: string) => {
         try {
             const response = await fetch(`${API_URL}/Auth/Login`, {
@@ -37,6 +47,32 @@ export const useLoginApi = () => {
                 expires: new Date(Date.now() + 1000 * 60 * 15), // 15 minutes
                 sameSite: true,
             });
+
+
+            const jwt:string = serviceResponse.data;
+            // console.log(jwt);
+            const userData = await fetch(`${API_URL}/Users/Self`, {
+                method: 'GET',
+                headers: headersCreator(jwt),
+            })
+            const userServiceResponse: ServiceResponse<User> = await userData.json();
+
+            if (!userServiceResponse.success) {
+                Swal.fire({
+                    title: 'Error!',
+                    text: userServiceResponse.message,
+                    icon: 'error',
+                    confirmButtonText: 'Confirm'
+                });
+                throw new Error(userServiceResponse.message);
+            }
+
+            if(userServiceResponse.data.authorizationLevel > 1){
+                setCookie(isAdminCookieName, true, {
+                    expires: new Date(Date.now() + 1000 * 60 * 15), // 15 minutes
+                    sameSite: true,
+                });
+            }
             // console.log(`Logged in successfully ${serviceResponse.data}`);
         } catch (error) {
             console.error('Login error:', error);
