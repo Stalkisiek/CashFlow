cebcc513e249289e4caff87ea0e0dd58a55a0e55
Stalkisiek <matmigdal@gmail.com>
2024-01-25 23:31:05 +0100

    Fixed alignment of titles

--- Frontend/cashflow/src/features/register/RegisterForm.tsx
+++ Frontend/cashflow/src/features/register/RegisterForm.tsx
@@ -1,4 +1,4 @@
-import {FC, useState} from "react";
+import React, {FC, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import logo from '../../pictures/logoWithoutBackground.png'
 import {useRegisterApi} from "./api";
